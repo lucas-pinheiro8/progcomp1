d91f0ff5a4e5230657898f292c74e7fc1f00a12c
Lucas Pinheiro <lucaspccacere.8@gmail.com>
2025-04-02 13:43:20 -0300

    primeiro site

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -2,45 +2,51 @@ function calcular(){
     //Vamos criar duas variáveis
     //JS as variáveis não possuem tipo
 
-    let mascote, homenagem, leite, kitAvulso, suplementoAvulso, arroz5Avulso, arroz1Avulso, feijao2Avulso, feijao1Avulso,
-    macarraoAvulso, oleoAvulso
+    let mascote, homenagem, leite, kit, suplemento, soma, equipe
     
     //recupera o valor do  mascote digitado pelo usuário
-    mascote = document.getElementById("mascote").value
+    mascote = Number(document.getElementById("mascote").value)
     
     //recupera o valor da homenagem digitado pelo usuário
-    homenagem = document.getElementById("homenagem").value
+    homenagem = Number(document.getElementById("homenagem").value)
     //recupera a quantidade de litros de leite
 
-    leite = document.getElementById("leite").value
+    leite = Number(document.getElementById("leite").value)
     
     //recuperar a quantidade avulsa de kits de alimentação
-    kitAvulso = document.getElementById("kitAvulso").value
+    kit = Number(document.getElementById("kit").value)
 
     //recuperar a quantidade avulsa de suplemento
-    suplementoAvulso = document.getElementById("suplementoAvulso").value
+    suplemento = Number(document.getElementById("suplemento").value)
 
-    arroz5Avulso = document.getElementById("arroz5Avulso").value
-
-    arroz1Avulso = document.getElementById("arroz1Avulso").value
-    
-    feijao2Avulso = document.getElementById("feijao2Avulso").value
-    
-    feijao1Avulso = document.getElementById("feijao1Avulso").value
-    
-    macarraoAvulso = document.getElementById("macarraoAvulso").value
-    
-    oleoAvulso = document.getElementById("oleoAvulso").value
+   
 
 
 
     //calcular a soma
-    let soma = Number(mascote) + Number(homenagem) + (2 * Number(leite)) + (30 * Number(kitAvulso)) + (15 * Number(suplementoAvulso)) +
-    (5 * Number(arroz5Avulso)) + (1 * Number(arroz1Avulso)) + (2 * Number(feijao2Avulso)) + (1 * Number(feijao1Avulso)) + 
-    (0.5 * Number(macarraoAvulso)) + (1 * Number(oleoAvulso))
+     soma = mascote + homenagem + (2 *(leite)) 
+
+    equipe = document.getElementById("equipe").value
+
+    if(equipe == "Laranja"){
+        if(kit >= 97){
+            soma = soma + 5000 + ((kit - 97) * 30)
+        }
+        else if(kit >= 78){
+            soma = soma + 4000 + ((kit - 78) * 30)
+        }
+        else if(kit >= 49){
+            soma = soma + 2500 + ((kit - 49) * 30)
+        }
+        else if(kit >= 19){
+            soma = soma + 1000 + ((kit - 19) * 30)
+        }
+    
+    }
+    
     
     //template string usa-se a crase
-    document.getElementById("soma").innerText = `A soma é ${soma}`
+    document.getElementById("soma").innerHTML = `A soma é ${soma}`
 
 
 
